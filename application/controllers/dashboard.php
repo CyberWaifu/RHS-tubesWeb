f34f00c59fdca6021428adc6f2e178d7b1507473
CyberWaifu <reihanxray@gmail.com>
2020-04-09 21:00:36 +0700

    menampilkan produk dari database + carousel

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -3,9 +3,10 @@
 class Dashboard extends CI_controller{
 
     public function index(){
+        $data['barang'] = $this->model_barang->tampil_data()->result();
         $this->load->view('templates/header');
         $this->load->view('templates/sidebar');
-        $this->load->view('pages/dashboard');
+        $this->load->view('pages/dashboard', $data);
         $this->load->view('templates/footer');
     }
 }
---
+++ application/views/pages/dashboard.php
@@ -0,0 +1,42 @@
+<div class="container-fluid">
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="<?php echo base_url('assets/img/slider1.jpg') ?>" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="<?php echo base_url('assets/img/slider2.jpg') ?>" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+  <div class="row text-center mt-5">
+
+    <?php foreach ($barang as $brg) : ?>
+      <div class="card ml-3" style="width: 16rem;">
+        <img src="<?php echo base_url().'/uploads/'.$brg->gambar ?>" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title mb-1"><?php echo $brg->nama_barang ?></h5>
+          <small><?php echo $brg->keterangan ?></small><br>
+          <span class="badge badge-success mb-3">Rp. <?php echo $brg->harga ?></span>
+          <a href="#" class="btn btn-sm btn-primary mb-2"><i class="fa fa-plus"></i>Tambah Ke keranjang</a>
+          <a href="#" class="btn btn-sm btn-success">Detail</a>
+        </div>
+      </div>
+    <?php endforeach ?>
+  </div>
+</div>
