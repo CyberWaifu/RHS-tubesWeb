b25540d00e3658d2c9923d48b89e407f7ee31674
CyberWaifu <reihanxray@gmail.com>
2020-04-12 20:45:33 +0700

    Membuat fitur delete dan update barang

--- application/controllers/admin/data_barang.php
+++ application/controllers/admin/data_barang.php
@@ -44,4 +44,44 @@ class Data_barang extends CI_Controller{
         $this->model_barang->tambah_barang($data,'tb_barang');
         redirect('admin/data_barang/index');
     }
+
+    public function edit($id){
+        $where = array('id_barang' =>$id);
+        $data['barang'] = $this->model_barang->edit_barang($where,'tb_barang')->result();
+        $this->load->view('templates-admin/header');
+        $this->load->view('templates-admin/sidebar');
+        $this->load->view('admin/edit_barang',$data);
+        $this->load->view('templates-admin/footer');
+    }
+
+    public function update(){
+        $id = $this->input->post('id_barang');
+        $nama_barang = $this->input->post('nama_barang');
+        $keterangan = $this->input->post('keterangan');
+        $kategori = $this->input->post('kategori');
+        $harga = $this->input->post('harga');
+        $stok = $this->input->post('stok');
+
+        $data = array(
+            'nama_barang' => $nama_barang,
+            'keterangan'  => $keterangan,
+            'kategori'    => $kategori,
+            'harga'       => $harga,
+            'stok'        => $stok
+        );
+
+        $where = array(
+            'id_barang' => $id
+        );
+
+        $this->model_barang->update_data($where,$data,'tb_barang');
+        redirect('admin/data_barang/index');
+    }
+
+    public function hapus($id){
+        $where = array('id_barang' => $id);
+        $this->model_barang->hapus_data($where,'tb_barang');
+        redirect('admin/data_barang/index');
+        
+    }
 }
